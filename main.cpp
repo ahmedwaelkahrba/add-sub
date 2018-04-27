e25afc2a9303620c86541f8db257daac49995de5
ahmedwaelkahrba <ahmedwaelkahrba@gmail.com>
2018-04-27 11:23:58 +0300

    source

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int add (int a,int b)
+{
+
+}
+int sub(int a,int b)
+{
+
+}
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+    cout << add(a,b);
+    cout<< sub(a,b);
+}
